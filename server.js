e47c588136bee7485a81030ae2ebe8fb3c8ee3ef
salwankajas <57811975+salwankajas@users.noreply.github.com>
2022-03-11 22:23:02 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const app = express();
+const path = require("path");
+const port = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+	res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+app.get('/script.js',(req, res) => {
+	res.sendFile(path.join(__dirname, 'script.js'));
+});
+
+app.listen(port);
+console.log("listenining on " + port);
